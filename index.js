01d5bc8f95479547d3de0fcf9c760692f5bebf21
Julia Nye <jujaritchie@gmail.com>
2024-01-18 09:23:16 -0700

    Class Activity Lesson 02

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
-
+const port = process.env.PORT || 3000;
 const routes = require("./routes/index");
+const mongodb = require('./db/connect');
 
 app.use('/', routes);
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-})
 
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log(`Connected to MongoDB successfully and listening on ${port}.`);
+    }
+  });
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
-const nameController = require('../controllers/names')
+// const nameController = require('../controllers/names')
+const userController = require('../controllers/user');
 
-router.get('/', (req, res) =>{
-    res.send(nameController.getMyName());
-});
+// router.get('/', (req, res) =>{
+//     res.send(nameController.getMyName());
+// });
 
-router.get('/sue', (req, res) =>{
-    res.send(nameController.getSue());
-});
+// router.get('/sue', (req, res) =>{
+//     res.send(nameController.getSue());
+// });
+
+router.get('/', userController.getUser);
+router.get('/username', userController.getUsername);
 
 module.exports = router;
